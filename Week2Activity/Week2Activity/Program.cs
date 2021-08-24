d82a5a75d7cfd64c7d95725ccf535c9b1598a288
Osiris Loera <loera.osiris@gmail.com>
2021-08-24 12:59:15 -0600

    First Commit

---
+++ Week2Activity/Week2Activity/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Week2Activity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student1 = new Student();
+            var student2 = new Student();
+            student1.Name = "Osiris loera";
+            student1.Major = "Web development";
+            student1.AddSubjects("English", "Intro to C#", "OOP", "Javascript");
+            student1.PrintStudentInfo();
+
+
+            Console.ReadLine();
+            student2.Name = "Mat";
+            student2.Major = "Economy";
+            student2.AddSubjects("intro to economy");
+            student2.PrintStudentInfo();
+        }
+    }
+}
